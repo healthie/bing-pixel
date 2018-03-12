66d23f61b503f663d064ae0a37a9e39c1c41346e
Manel García <manelet@gmail.com>
2018-03-12 13:36:28 +0100

    Fixing SSR problems

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: 'index.js',
-        libraryTarget: "umd",
+        libraryTarget: "var",
         library: "createBingPixel"
     },
     module: {
