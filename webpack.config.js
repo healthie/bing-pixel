84003747c4f5f9273bbc33fae493a015ad54b525
Manel García <manelet@gmail.com>
2018-03-12 10:19:54 +0100

    Changing way to export to avoid window is not defined error

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: 'index.js',
-        libraryTarget: "umd",
-        library: "BingPixel"
+        filename: 'index.js'
     },
     module: {
         rules: [
